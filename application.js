6b2a571f59b354c0f02d1d9c333ed1a9cc9e0a72
John <johnmarksjr@gmail.com>
2014-01-08 18:17:47 -0700

    Got logic working in the game class

--- application.js
+++ application.js
@@ -1,22 +1,40 @@
 // Game object constructor
 function Game(lb, ub) {
+	// A bunch of private state
 	var lowerBounds = lb;
 	var upperBounds = ub;
 	var hiddenNumber = 0;
 	var guess = 0;
-	var answer = ""; // "VERY COLD", "COLD", "WARM", "VERY WARM", "HOT", "VERY HOT"
+	var previousGuess = 0;
+	//var answer = ""; // "VERY COLD", "COLD", "WARM", "VERY WARM", "HOT", "VERY HOT"
 	var numGuesses = 0;
 	var guessesStr = "";
+	var catagories = {};
 
 	// Compute a random integer between upper and lower bounds
-
 	var computeHiddenNumber = function() {
 		hiddenNumber = Math.floor(Math.random() * 100) + 1;		
 	}
 
-	// Validate the guess
+	var computeAnswerCatagories = function() {
+		// There are seven catagories of possible answers, depending
+		// upon how far the answer is from the real value.
+//0*********1********2*********3*********4*********5*********6*********7*********8*********9
+//    |     |    |             |                   |                    
+// VH    H    VW         W               C                            VC
+       var spread = (upperBounds+1) - lowerBounds;
+       catagories.FOUND =     {dist:0,           color:'#00ff00', ans:'FOUND'};
+       catagories.VERY_HOT =  {dist:5,           color:'#ff0000', ans:'VERY HOT'};
+       catagories.HOT =       {dist:spread*0.1,  color:'#cc0000', ans:'HOT'};
+       catagories.VERY_WARM = {dist:spread*0.15, color:'#ff6f00', ans:'VERY WARM'};
+       catagories.WARM =      {dist:spread*0.3,  color:'#ff9c00', ans:'WARM'};
+       catagories.COLD =      {dist:spread*0.5,  color:'#4387fd', ans:'COLD'};
+       catagories.VERY_COLD = {dist:spread*0.5,  color:'#0000ff', ans:'VERY COLD'};
+	}
+
+	// Validate the guess: Is it empty? Is it a number? Is it within the game bounds
 	this.validateGuess = function(guessStr) {
-		console.log("guess = " + guessStr + " , a " + typeof(guessStr));
+		//console.log("guess = " + guessStr + " , a " + typeof(guessStr));
 		if(guessStr === "") return; // Don't want empty guesses
 		if (!$.isNumeric(guessStr)) { // Only want real numbers - thank you jQuery!
 			alert("Please enter a number!");
@@ -27,6 +45,7 @@ function Game(lb, ub) {
 			alert("Please enter a number between " + lowerBounds + " and " + upperBounds);
 			return;
 		}
+		previousGuess = guess;
 		guess = guessNum;
 		numGuesses++;
 		if (guessesStr.length != 0)
@@ -35,16 +54,64 @@ function Game(lb, ub) {
 		return true;
 	}
 
+	// Figure out what band of warmth or coldness the current guess lies
+	var getCatagoryIndex = function() {
+		var diff = hiddenNumber - guess;
+		if (diff < 0) diff = -diff;
+
+		if (diff == catagories['FOUND']['dist']) {
+			ans = 'FOUND';
+		} else if (diff <= catagories['VERY_HOT']['dist']) {
+			ans = 'VERY_HOT';	
+		} else if (diff <= catagories['HOT']['dist']) {
+			ans = 'HOT';
+		} else if (diff <= catagories['VERY_WARM']['dist']) {
+			ans = 'VERY_WARM';
+		} else if (diff <= catagories['WARM']['dist']) {
+			ans = 'WARM';
+		} else if (diff <= catagories['COLD']['dist']) {
+			ans = 'COLD';
+		} else {
+			ans = 'VERY_COLD';
+		}
+		return ans;
+	}
+
+	// Figure out if the current guess is warmer or colder than the previous guess
+	this.computeWarmer = function(previousAnswer) {
+		var index = getCatagoryIndex();
+		var ans = catagories[index]['ans'];
+
+		var diff = hiddenNumber - guess;
+		diff = (diff < 0) ? -diff : diff;
+		var prevDiff = hiddenNumber - previousGuess;
+		prevDiff = (prevDiff < 0) ? -prevDiff : prevDiff;
+
+		if (previousAnswer.length != 0) {
+			if (previousAnswer === ans) {
+				if (prevDiff > diff)
+					return 'warmer';
+				else if (prevDiff < diff)
+					return 'colder';
+				else
+					return 'same';
+			}
+		}
+		return "-";
+	}
+
 	// Returns an answer string depending upon the distance
 	// the guess is from the hiddenNumber
-	this.computeAnswer = function(guess) {
-		return "VERY COLD!";
+	this.computeAnswer = function() {
+		var index = getCatagoryIndex();
+		return catagories[index]['ans'];
 	}
 
 	// Returns a color value depending upon the distance the
 	// guess is from the hiddenNumber
-	this.computeAnsColor = function(guess) {
-		return "#4387fd"
+	this.computeAnsColor = function() {
+		var index = getCatagoryIndex();
+		return catagories[index]['color'];
 	}
 
 	this.getGuessesStr = function() {
@@ -57,6 +124,7 @@ function Game(lb, ub) {
 
 	// Init code here
 	computeHiddenNumber();
+	computeAnswerCatagories();
 }
 
 $(document).ready(function() {
@@ -64,28 +132,35 @@ $(document).ready(function() {
 	var game = new Game(1, 100);
 
 	// On return:
-	// * grab contents of the input box
+	// * grab contents of the input box on the "return" keypress
 	// * validate the input (is it a number, within the bounds, etc)
+	// * update the list of previous guesses and write to the DOM
 	// * get the answer text
 	// * get the color value of the answer text
 	// * write the answer text into the DOM using the computed color
 	// * clear out the input field
 	$("#guess").keypress(function(event) {
 		if (event.which != 13) return;
-		var guess = $(this).val();
+		try {
+			if (!game.validateGuess($(this).val())) {
+				throw "Not valid guess";
+			}
 
-		if (!game.validateGuess(guess))
-			return;
+			$('center ul li.num_guesses').html("(" + game.getNumGuesses() + ")");
+			$('center ul li.ans_number').html(game.getGuessesStr());
 
-		$('center ul li.num_guesses').html("(" + game.getNumGuesses() + ")");
-		$('center ul li.ans_number').html(game.getGuessesStr());
+			$('center p#answer').css('color', game.computeAnsColor());
+			var previousAnswer = $('center p#answer').html();
+			$('center p#answer').html(game.computeAnswer());
 
-		$('center p#answer').css('color', game.computeAnsColor());
-		$('center p#answer').html(game.computeAnswer());
-
-		event.preventDefault(); // Keep input from from doing default action, like clearing
-		$(this).val(""); // Clear the input form
-		console.log(event.which);
+			$('center p#warmer').html(game.computeWarmer(previousAnswer));
+		} catch(e) {
+			//console.log(e);
+		} finally {
+			event.preventDefault();
+			event.stopPropagation();
+			$(this).val(""); // Clear the input form
+		}
 	});
 });
 
